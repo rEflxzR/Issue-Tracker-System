0b1362e1d0d3c4d3b617141870eb2a60b7a5184b
rEflxzR <anshu.singh1429@gmail.com>
2021-03-16 22:06:10 +0530

    Manage User Page UI Complete

--- src/Components/Dashboardcomponents/homepage.js
+++ src/Components/Dashboardcomponents/homepage.js
@@ -7,12 +7,12 @@ class Homepage extends Component {
             <div className="d-flex justify-content-center">
                 <div style={{ width: '80vw' }}>
                     <div className="row d-flex justify-content-between px-5 mx-5" style={{ height: '50vh' }}>
-                        <div className="col col-5 px-0">
+                        {/* <div className="col col-5 px-0">
                             <Box />
                         </div>
                         <div className="col col-5 px-0">
                             <Box />
-                        </div>
+                        </div> */}
                     </div>
                     {/* <div className="row d-flex justify-content-between px-5 mx-5">
                         <div className="col col-5 px-0">
--- src/Components/Dashboardcomponents/manageusers.js
+++ src/Components/Dashboardcomponents/manageusers.js
@@ -1,32 +1,36 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-import Input from '@material-ui/core/Input';
+import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import ListItemText from '@material-ui/core/ListItemText';
-import Select from '@material-ui/core/Select';
-import Checkbox from '@material-ui/core/Checkbox';
-import Chip from '@material-ui/core/Chip';
+import Select from '@material-ui/core/Select'
+import Button from '@material-ui/core/Button'
 import Box from '../Auxcomponents/genericbox'
+import './manageusers.css'
 
 class ManageUsers extends Component {
     constructor(props) {
         super(props)
         this.state = {
             userData: [],
-            names: ["ReflxzR", "ReflxzR", "ReflxzR", "ReflxzR"],
-            personName: ""
+            names: ["ReflxzR", "rEplan", "JanzWE", "qLimAxz", "MAC", "sLWx"],
+            personName: [],
+            personRole: "",
+            currentPageNumber: 1,
+            update: false
         }
 
-        this.handleChangeMultiple = this.handleChangeMultiple.bind(this)
+        this.handleSubmitClick = this.handleSubmitClick.bind(this)
+        this.handleClearClick = this.handleClearClick.bind(this)
+        this.handleRoleAssignmentChange = this.handleRoleAssignmentChange.bind(this)
+        this.handleRoleMenuChange = this.handleRoleMenuChange.bind(this)
+        this.pageToggle = this.pageToggle.bind(this)
     }
 
     async componentDidMount() {
         const url = `http://${window.location.hostname}:8000/users`
         await axios.get(url)
         .then((res) => {
-            console.log(res.data)
             this.setState({ userData: res.data })
         })
         .catch((err) => {
@@ -35,38 +39,100 @@ class ManageUsers extends Component {
         })
     }
 
-    handleChangeMultiple() {
+    async componentDidUpdate(prevProps, prevState) {
+        if(prevState.update!==this.state.update) {
+            const url = `http://${window.location.hostname}:8000/users`
+            await axios.get(url)
+            .then((res) => {
+                this.setState({ userData: res.data, update: false })
+            })
+            .catch((err) => {
+                console.log(err)
+                console.log("Request Failed")
+            })
+        }
+    }
 
+    pageToggle(param) {
+        this.setState({ currentPageNumber: param })
+    }
+
+    handleRoleAssignmentChange(evt) {
+        const { options } = evt.target
+        const values = []
+        for(let option of options) {
+            if(option.selected) {
+                values.push(option.value)
+            }
+        }
+        this.setState({ personName: values })
+    }
+
+    handleClearClick() {
+        this.setState({ personName: [] })
+    }
+
+    handleRoleMenuChange(evt) {
+        this.setState({ personRole: evt.currentTarget.getAttribute("data-value") })
+    }
+
+    async handleSubmitClick() {
+        const url = `http://${window.location.hostname}:8000/updateusers`
+        const { personName, personRole } = this.state
+        await axios.post(url, {personName, personRole})
+        .then((res) => {
+            this.setState({ personName: [], personRole: "", update: true })
+        })
+        .catch((err) => {
+            console.log("Error Sending the Request to the Server")
+            console.log(err)
+        })
     }
 
     render() {
+        const { userData, currentPageNumber } = this.state
         return(
             <div>
-                <div className="row" style={{ height: '70vh', width: '80vw' }}>
+                <div className="row mainrow">
                     <div className="col col-4">
-                        {/* <FormControl>
-                        <InputLabel shrink htmlFor="select-multiple-native">
-                            Native
-                            </InputLabel>
-                            <Select
-                            multiple
-                            native
-                            value={this.state.personName}
-                            onChange={this.handleChangeMultiple}
-                            inputProps={{
-                                id: 'select-multiple-native',
-                            }}
-                            >
-                            {this.state.names.map((name) => (
-                                <option key={name} value={name}>
-                                {name}
-                                </option>
-                            ))}
-                            </Select>
-                        </FormControl> */}
+                        <div className="form">
+                            <form>
+                                <h2 className="my-5 text-center">Manage User Roles</h2>
+                                <FormControl required className="selectmenu">
+                                    <InputLabel shrink>Select 1 or more Users</InputLabel>
+                                    <Select disabled={window.localStorage.getItem("role")==="admin" ? false : true} multiple native value={this.state.personName} onChange={this.handleRoleAssignmentChange}>
+                                        {
+                                            this.state.userData.map((user) => {
+                                                return <option key={user.username} value={user.username}>{user.username}</option>
+                                            })
+                                        }
+                                    </Select>
+                                </FormControl>
+                                <div className="my-3">
+                                    <FormControl required className="rolemenu">
+                                        <InputLabel id="demo-simple-select-label">Select A Role to Assign</InputLabel>
+                                        <Select labelId="demo-simple-select-label" value={this.state.personRole} onChange={this.handleRoleMenuChange}>
+                                            <MenuItem value="admin">Admin</MenuItem>
+                                            <MenuItem value="projectmanager">Project Manager</MenuItem>
+                                            <MenuItem value="developer">Developer</MenuItem>
+                                            <MenuItem value="tester">Tester</MenuItem>
+                                        </Select>
+                                    </FormControl>
+                                </div>
+
+                                <div className="formbuttons my-4">
+                                    <Button disabled={window.localStorage.getItem("role")==="admin" ? false : true} onClick={this.handleClearClick} size="large" variant="contained" color="primary">Clear Choices</Button>
+                                    <Button disabled={window.localStorage.getItem("role")==="admin" ? false : true} onClick={this.handleSubmitClick} size="large" variant="contained" color="primary">Submit</Button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                     <div className="col col-8">
-                        <Box users={this.state.userData} />
+                        {
+                            [...Array(currentPageNumber)].map((e, index) => {
+                                return currentPageNumber==index+1 && <Box title="All Users" users={userData.slice(currentPageNumber*10-10, currentPageNumber*10)} togglePage={this.pageToggle} totalEntries={userData.length} page={currentPageNumber} />
+                            })
+                        }
                     </div>
                 </div>
             </div>
